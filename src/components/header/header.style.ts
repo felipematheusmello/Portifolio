6cfde6bd662a4964061481ac33cacab9c96c93d3
Felipe <felipematheusmellodemorais@gmail.com  name = Felipe Matheus Mello de Morais>
2020-10-22 09:22:07 -0300

    style: titles are updated

--- src/components/header/header.style.ts
+++ src/components/header/header.style.ts
@@ -2,19 +2,28 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 export const StyledLink = styled(Link)`
-margin-left:30px;
-text-decoration:none;
-color: ${props => props.color};
-font-size:17px;
-font-family: 'Roboto', sans-serif;
+    margin-left:30px;
+    text-decoration:none;
+    color: ${props => props.color};
+    font-size:17px;
+    font-family: 'Roboto', sans-serif;
 `
 export const Container = styled.div`
-display:flex;
-flex-flow:wrap row;
-align-items:center;
-justify-content:right;
-padding-bottom:10px;
-border-bottom: solid 1px #ddd; 
-width:100%;
-height:40px;
+    display:flex;
+    flex-flow:nowrap row;
+    align-items:center;
+    justify-content:space-around;
+    padding-bottom:10px;
+    border-bottom: solid 1px #ddd; 
+    width:100vw;
+    height:40px;
+`
+
+export const Title = styled.h1` 
+    margin:0;
+    color:#FFFF;
+    font-size:30px;
+    font-family: 'Roboto', sans-serif;
+    font-weight:bold;
+    letter-spacing:2px;
 `
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import { StyledLink, Container } from './header.style'
+import { StyledLink, Container, Title } from './header.style'
 import { useLocation } from 'react-router-dom'
 const Header = () => {
     const { pathname } = useLocation()
     return (
         <Container>
+
+            <Title>Felipe</Title>
             <StyledLink to='/' color={pathname === '/' ? '#5398BE' : '#FFFF'} >SOBRE</StyledLink>
             <StyledLink to='/tecnologies' color={pathname === '/tecnologies' ? '#5398BE' : '#FFFF'}>TECNOLOGIAS</StyledLink>
             <StyledLink to='/projects' color={pathname === '/projects' ? '#5398BE' : '#FFFF'}>PROJETOS</StyledLink>
